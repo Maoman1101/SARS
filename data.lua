f5aaaeb0dbe11828f6b31414d32dcc9a6ae59845
Maoman1101 <Taylormao+github@gmail.com>
2025-09-22 11:42:26 -0400

    Update to 1.4.7

--- data.lua
+++ data.lua
@@ -21,7 +21,8 @@ local function make_alt_recipe(base_name, alt_suffix, ingredients, results, ener
     group = "sars",
     subgroup = "sars-alt-recipes",
     order = base_recipe and base_recipe.order,
-    sars_alt = true
+    sars_alt = true,
+	auto_recycle = false
   }
 
   if base_recipe then
@@ -66,17 +67,17 @@ table.insert(recipes, make_alt_recipe("iron-plate", "pure-iron",
 table.insert(recipes, make_alt_recipe("iron-plate", "iron-alloy",
   { {type = "item", name = "iron-ore", amount = 8}, {type = "item", name = "copper-ore", amount = 2} },
   { {type = "item", name = "iron-plate", amount = 15} },
-  0.32, "advanced-crafting"))
+  0.8, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("iron-plate", "inferior-iron-plate",
   { {type = "item", name = "iron-plate", amount = 3}, {type = "item", name = "plastic-bar", amount = 1} },
   { {type = "item", name = "iron-plate", amount = 5} },
-  0.8, "advanced-crafting"))
+  1.6, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("iron-plate", "leeched-iron-plate",
   { {type = "item", name = "iron-ore", amount = 5}, {type = "fluid", name = "sulfuric-acid", amount = 1} },
   { {type = "item", name = "iron-plate", amount = 10} },
-  0.4, "crafting-with-fluid"))
+  0.8, "crafting-with-fluid"))
 
 table.insert(recipes, make_alt_recipe("copper-plate", "pure-copper",
   { {type = "item", name = "copper-ore", amount = 2}, {type = "fluid", name = "water", amount = 1} },
@@ -86,22 +87,22 @@ table.insert(recipes, make_alt_recipe("copper-plate", "pure-copper",
 table.insert(recipes, make_alt_recipe("copper-plate", "copper-alloy",
   { {type = "item", name = "copper-ore", amount = 8}, {type = "item", name = "iron-ore", amount = 2} },
   { {type = "item", name = "copper-plate", amount = 15} },
-  0.32, "advanced-crafting"))
+  0.8, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("copper-plate", "crappy-copper-plate",
   { {type = "item", name = "copper-plate", amount = 3}, {type = "item", name = "plastic-bar", amount = 1} },
   { {type = "item", name = "copper-plate", amount = 5} },
-  0.8, "advanced-crafting"))
+  1.6, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("copper-plate", "leeched-copper-plate",
   { {type = "item", name = "copper-ore", amount = 5}, {type = "fluid", name = "sulfuric-acid", amount = 1} },
   { {type = "item", name = "copper-plate", amount = 10} },
-  0.4, "crafting-with-fluid"))
+  0.8, "crafting-with-fluid"))
 
 table.insert(recipes, make_alt_recipe("steel-plate", "carbonized-steel",
   { {type = "item", name = "iron-plate", amount = 4}, {type = "item", name = "coal", amount = 1} },
   { {type = "item", name = "steel-plate", amount = 1} },
-  16, "advanced-crafting"))
+  6.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("steel-plate", "steel-alloy",
   { {type = "item", name = "iron-plate", amount = 8}, {type = "item", name = "copper-plate", amount = 2} },
@@ -111,7 +112,7 @@ table.insert(recipes, make_alt_recipe("steel-plate", "steel-alloy",
 table.insert(recipes, make_alt_recipe("iron-stick", "steel-stick",
   { {type = "item", name = "steel-plate", amount = 1} },
   { {type = "item", name = "iron-stick", amount = 12} },
-  0.1))
+  2.0))
 
 table.insert(recipes, make_alt_recipe("copper-cable", "iron-cable",
   { {type = "item", name = "iron-plate", amount = 5} },
@@ -121,7 +122,7 @@ table.insert(recipes, make_alt_recipe("copper-cable", "iron-cable",
 table.insert(recipes, make_alt_recipe("copper-cable", "steel-cable",
   { {type = "item", name = "steel-plate", amount = 1} },
   { {type = "item", name = "copper-cable", amount = 12} },
-  0.1, "advanced-crafting"))
+  2.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("concrete", "dry-concrete",
   { {type = "item", name = "stone", amount = 3} },
@@ -129,14 +130,14 @@ table.insert(recipes, make_alt_recipe("concrete", "dry-concrete",
   5.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("refined-concrete", "ultra-processed-concrete",
-  { {type = "item", name = "concrete", amount = 1} },
+  { {type = "item", name = "concrete", amount = 2} },
   { {type = "item", name = "refined-concrete", amount = 1} },
   25.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("iron-gear-wheel", "cast-iron-gear",
   { {type = "item", name = "steel-plate", amount = 1} },
   { {type = "item", name = "iron-gear-wheel", amount = 10} },
-  0.1))
+  2.0))
 
 table.insert(recipes, make_alt_recipe("iron-gear-wheel", "alloyed-gear",
   { {type = "item", name = "iron-plate", amount = 3}, {type = "item", name = "copper-plate", amount = 1} },
@@ -146,27 +147,22 @@ table.insert(recipes, make_alt_recipe("iron-gear-wheel", "alloyed-gear",
 table.insert(recipes, make_alt_recipe("engine-unit", "electric-rotor",
   { {type = "item", name = "iron-stick", amount = 1}, {type = "item", name = "copper-cable", amount = 8}, {type = "item", name = "pipe", amount = 2} },
   { {type = "item", name = "engine-unit", amount = 1} },
-  1, "advanced-crafting"))
+  1.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("engine-unit", "cheap-quality-engine",
   { {type = "item", name = "iron-gear-wheel", amount = 1}, {type = "item", name = "pipe", amount = 2}, {type = "item", name = "plastic-bar", amount = 2}, {type = "item", name = "copper-plate", amount = 1} },
   { {type = "item", name = "engine-unit", amount = 1} },
-  0.5, "advanced-crafting"))
+  2.5, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("engine-unit", "diesel-engines",
   { {type = "fluid", name = "heavy-oil", amount = 1}, {type = "item", name = "pipe", amount = 2}, {type = "item", name = "steel-plate", amount = 3}, {type = "item", name = "iron-gear-wheel", amount = 2} },
   { {type = "item", name = "engine-unit", amount = 2} },
-  0.5, "crafting-with-fluid"))
+  3.5, "crafting-with-fluid"))
 
 table.insert(recipes, make_alt_recipe("plastic-bar", "crude-plastic",
   { {type = "fluid", name = "crude-oil", amount = 1}, {type = "item", name = "coal", amount = 2} },
   { {type = "item", name = "plastic-bar", amount = 1} },
-  0.25, "chemistry"))
-
-table.insert(recipes, make_alt_recipe("plastic-bar", "microplastic-filtering",
-  { {type = "item", name = "raw-fish", amount = 10} },
-  { {type = "item", name = "plastic-bar", amount = 1}, {type = "item", name = "spoilage", amount = 10} },
-  5.0, "chemistry"))
+  2.0, "chemistry"))
 
 recipes[#recipes].icon = "__base__/graphics/icons/plastic-bar.png"
 recipes[#recipes].icon_size = 64
@@ -174,7 +170,7 @@ recipes[#recipes].icon_size = 64
 table.insert(recipes, make_alt_recipe("battery", "inefficient-battery",
   { {type = "item", name = "sulfur", amount = 1}, {type = "item", name = "iron-plate", amount = 3}, {type = "item", name = "copper-plate", amount = 2} },
   { {type = "item", name = "battery", amount = 1} },
-  1.0))
+  1.0, "advanced-crafting"))
 
 table.insert(recipes, make_alt_recipe("battery", "smart-battery",
   { {type = "item", name = "iron-plate", amount = 1}, {type = "item", name = "copper-plate", amount = 1}, {type = "fluid", name = "sulfuric-acid", amount = 10}, {type = "item", name = "advanced-circuit", amount = 1} },
@@ -184,20 +180,84 @@ table.insert(recipes, make_alt_recipe("battery", "smart-battery",
 table.insert(recipes, make_alt_recipe("electronic-circuit", "etched-circuits",
   { {type = "item", name = "iron-plate", amount = 1}, {type = "fluid", name = "sulfuric-acid", amount = 10} },
   { {type = "item", name = "electronic-circuit", amount = 1} },
-  0.5, "crafting-with-fluid"))
+  2.0, "crafting-with-fluid"))
 
 table.insert(recipes, make_alt_recipe("advanced-circuit", "etched-adv-circuits",
   { {type = "item", name = "electronic-circuit", amount = 2}, {type = "item", name = "plastic-bar", amount = 2}, {type = "fluid", name = "sulfuric-acid", amount = 10} },
   { {type = "item", name = "advanced-circuit", amount = 1} },
-  1, "crafting-with-fluid"))
+  4.0, "crafting-with-fluid"))
+  
+table.insert(recipes, make_alt_recipe("coal", "charcoal",
+  { {type = "item", name = "wood", amount = 3} },
+  { {type = "item", name = "coal", amount = 2} },
+  10.0, "advanced-crafting"))
+
+table.insert(recipes, make_alt_recipe("electronic-circuit", "plastic-circuits",
+  { {type = "item", name = "plastic-bar", amount = 1}, {type = "item", name = "copper-cable", amount = 3} },
+  { {type = "item", name = "electronic-circuit", amount = 1} },
+  0.5, "advanced-crafting"))
+
+table.insert(recipes, make_alt_recipe("lubricant", "water-based-lubricant",
+  { {type = "fluid", name = "water", amount = 10}, {type = "fluid", name = "petroleum-gas", amount = 10} },
+  { {type = "fluid", name = "lubricant", amount = 10} },
+  1.0, "chemistry"))
+
+table.insert(recipes, make_alt_recipe("low-density-structure", "quick-and-dirty-structure",
+  { {type = "item", name = "copper-plate", amount = 25}, {type = "item", name = "plastic-bar", amount = 8},
+    {type = "item", name = "steel-plate", amount = 3}, {type = "item", name = "coal", amount = 1} },
+  { {type = "item", name = "low-density-structure", amount = 1} },
+  5.0, "advanced-crafting"))
+
+table.insert(recipes, make_alt_recipe("uranium-fuel-cell", "trace-uranium-filtering",
+  { {type = "item", name = "uranium-238", amount = 100}, {type = "item", name = "coal", amount = 1} },
+  { {type = "item", name = "uranium-235", amount = 1} },
+  60.0, "advanced-crafting"))
+
+table.insert(recipes, make_alt_recipe("rocket-fuel", "electrolysis-fuel",
+  { {type = "fluid", name = "water", amount = 100}, {type = "fluid", name = "light-oil", amount = 10} },
+  { {type = "item", name = "rocket-fuel", amount = 1} },
+  2.0, "chemistry"))
+
+table.insert(recipes, make_alt_recipe("explosives", "nitroglycerin",
+  { {type = "fluid", name = "light-oil", amount = 1}, {type = "item", name = "sulfur", amount = 1}, {type = "fluid", name = "water", amount = 10} },
+  { {type = "item", name = "explosives", amount = 2} },
+  4.0, "chemistry"))
+
+table.insert(recipes, make_alt_recipe("explosives", "plastic-c4",
+  { {type = "fluid", name = "petroleum-gas", amount = 1}, {type = "item", name = "sulfur", amount = 1}, {type = "item", name = "plastic-bar", amount = 1} },
+  { {type = "item", name = "explosives", amount = 3} },
+  5.0, "chemistry"))
+
+table.insert(recipes, make_alt_recipe("flying-robot-frame", "lightweight-robot-frame",
+  { {type = "item", name = "plastic-bar", amount = 2}, {type = "item", name = "battery", amount = 2},
+    {type = "item", name = "electronic-circuit", amount = 2}, {type = "item", name = "electric-engine-unit", amount = 1} },
+  { {type = "item", name = "flying-robot-frame", amount = 1} },
+  20.0, "advanced-crafting"))
+
+table.insert(recipes, make_alt_recipe("electric-engine-unit", "dry-electric-engine",
+  { {type = "item", name = "electronic-circuit", amount = 2}, {type = "item", name = "engine-unit", amount = 1}, {type = "item", name = "coal", amount = 1} },
+  { {type = "item", name = "electric-engine-unit", amount = 1} },
+  10.0, "advanced-crafting"))
 
 data:extend(recipes)
 
-data:extend({
+data:extend({ -- debug commands
   {
     type = "custom-input",
     name = "sars-force-unlock",
     key_sequence = "CONTROL + ALT + U",
+    consuming = "none"
+  },
+  {
+    type = "custom-input",
+    name = "sars-reset-alt-recipes",
+    key_sequence = "CONTROL + ALT + Y",
+    consuming = "none"
+  },
+  {
+    type = "custom-input",
+    name = "sars-open-manual-unlock",
+    key_sequence = "CONTROL + ALT + J",
     consuming = "none"
   }
 })
